4e3b7d68cb2fed6a3e94ec9b1bb20d9dd52de907
Servando Hernandez <s_musicpia@yahoo.com.mx>
2026-01-08 19:06:57 -0600

    Add DataAnnotation on Movie Entity

--- RazorPagesMovie/Migrations/20260109003603_AddDataAnnotationsMovieModel.Designer.cs
+++ RazorPagesMovie/Migrations/20260109003603_AddDataAnnotationsMovieModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie.Data;
 
@@ -11,9 +12,11 @@ using RazorPagesMovie.Data;
 namespace RazorPagesMovie.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20260109003603_AddDataAnnotationsMovieModel")]
+    partial class AddDataAnnotationsMovieModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -58,7 +61,7 @@ namespace RazorPagesMovie.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Movie", (string)null);
+                    b.ToTable("Movie");
                 });
 #pragma warning restore 612, 618
         }
